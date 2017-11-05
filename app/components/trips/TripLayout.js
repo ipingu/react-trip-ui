a76bd6436bbe9a0a36745cbf950351e68f9e32d6
ipingu <erik@clairiot.com>
2017-11-05 08:01:25 -0500

    Horizontal timeline instead of vertical

--- app/components/trips/TripLayout.js
+++ app/components/trips/TripLayout.js
@@ -6,19 +6,29 @@ import FontAwesome from "react-fontawesome";
 class TripLayout extends React.Component {
     render() {
         return (
+            <div>
+            <div className="horizontal-timeline">
+           {/*} <button>
+                        <FontAwesome name="home" size="2x" />
+                    </button>
+        */}      <button>Tokyo</button>
+                    <button>Shanghai</button>
+                    <button>Hiroshima</button>
+            </div>
             <div className="trip-enriched">
-                <div className="timeline">
+               {/* <div className="timeline">
                     <button>
                         <FontAwesome name="home" size="2x" />
                     </button>
                     <button>Tokyo</button>
                     <button>Shanghai</button>
                     <button>Hiroshima</button>
-                </div>
+        </div>*/}
                 <Trip {...this.props} />
                 <div className="metadata">
                     <Diaporama />
                 </div>
+            </div>
             </div>
         )
     }
